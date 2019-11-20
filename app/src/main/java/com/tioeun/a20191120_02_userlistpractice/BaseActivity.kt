6528bf98ce8e06e896b2fd80d33e387cd9dda447
YongMinSun <ddoni1010@gmail.com>
2019-11-20 20:55:18 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191120_02_userlistpractice/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.tioeun.a20191120_02_userlistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var mContext = this
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tioeun/a20191120_02_userlistpractice/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tioeun.a20191120_02_userlistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
