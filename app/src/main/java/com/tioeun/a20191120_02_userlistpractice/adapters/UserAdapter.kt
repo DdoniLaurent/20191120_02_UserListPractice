4ed2ec768d388623827a905a31f3153db8407c8a
YongMinSun <ddoni1010@gmail.com>
2019-11-20 21:51:16 +0900

    서버에서 사용자 목록 받기 / 어댑터 기초 작업 => 다른프로젝트에서 복사 / 붙여넣기 기반으로

---
+++ app/src/main/java/com/tioeun/a20191120_02_userlistpractice/adapters/UserAdapter.kt
@@ -0,0 +1,33 @@
+package com.tioeun.a20191119_01_banklistpractice.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.tioeun.a20191120_02_userlistpractice.R
+import com.tioeun.a20191120_02_userlistpractice.datas.User
+import java.util.ArrayList
+
+class UserAdapter(context: Context, res : Int, list:ArrayList<User>) : ArrayAdapter<User>(context,res,list) {
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    constructor(context: Context, list:ArrayList<User>) : this(context, R.layout.user_list_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.user_list_item, null)
+        }
+
+        var row = tempRow!!
+
+
+
+        return row
+    }
+}
--- app/src/main/java/com/tioeun/a20191120_02_userlistpractice/datas/Category.kt
+++ app/src/main/java/com/tioeun/a20191120_02_userlistpractice/datas/Category.kt
@@ -1,8 +1,20 @@
 package com.tioeun.a20191120_02_userlistpractice.datas
 
+import org.json.JSONObject
+
 class Category {
 
     var id = 0
     var title = ""
     var color = ""
+    
+    companion object {
+        fun getCategoryFromJson(cateJson:JSONObject) : Category {
+            var category = Category()
+            category.id = cateJson.getInt("id")
+            category.title = cateJson.getString("title")
+            category.color = cateJson.getString("color")
+            return category
+        }
+    }
 }
